c038c3a61dd3251990d8df06680b5c1a6a6b940b
Emilio Velasco <emilio_785@hotmail.com>
2025-01-28 22:58:41 -0600

    feat: get all todos

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -4,14 +4,21 @@ import { faker } from "@faker-js/faker";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const max = Number(searchParams.get("max"));
+  const max = Number(searchParams.get("max") ?? "20");
 
-  const maximumNumberOfRows = isNaN(max) ? 20 : max;
+  if (isNaN(max)) {
+    return NextResponse.json(
+      {
+        message: "Invalid query param",
+      },
+      { status: 400 }
+    );
+  }
 
   await prisma.todo.deleteMany();
 
   await prisma.todo.createMany({
-    data: Array.from({ length: maximumNumberOfRows }).map(() => ({
+    data: Array.from({ length: max }).map(() => ({
       description: faker.lorem.lines({ min: 1, max: 3 }),
       complete: faker.datatype.boolean(),
     })),
---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,26 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take) || isNaN(skip)) {
+    return NextResponse.json(
+      {
+        message: "Invalid query param",
+      },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
+
+  return NextResponse.json({
+    todos,
+  });
+}
